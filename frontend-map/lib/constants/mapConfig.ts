7384dbbfb975d53c4c502480c3d01c0a317959fd
KrishnaPatel007 <krishnaptl3127@gmail.com>
2026-08-21 15:46:02 +0530

    feat(types): define Camera, Auth, Filter models and Gujarat GIS constants

---
+++ frontend-map/lib/constants/mapConfig.ts
@@ -0,0 +1,6 @@
+import { LatLngExpression } from 'leaflet';
+
+export const GUJARAT_CENTER: LatLngExpression = [22.2587, 71.1924];
+export const DEFAULT_ZOOM = 7.5;
+export const CARTO_DARK_TILES = 'https://{s}.basemaps.cartocdn.com/dark_all/{z}/{x}/{y}{r}.png';
+export const CARTO_ATTRIBUTION = '&copy; <a href="https://carto.com/">CartoDB</a>';
